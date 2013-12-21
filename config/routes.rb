13545ee91216670d5d8eb1ff91637a625e683663
neil patel <neil@myindianweddingapp.com>
2013-12-21 11:55:55 +0000

    Created Home, About & Contact pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Pay::Application.routes.draw do
+  root "pages#home"
+  get "about" => "pages#about"      #creates about path
+  get "contact" => "pages#contact"  #creates contact path
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
